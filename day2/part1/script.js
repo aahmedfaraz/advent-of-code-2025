bf90731852b42da7b15a7ba81c4985648ee6f1f7
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2025-12-10 08:01:16 +0500

    solved day 2 part 1 puzzle

---
+++ day2/part1/script.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// Read file synchronously
+// const data = fs.readFileSync('sample-input.txt', 'utf-8');
+const data = fs.readFileSync('main-input.txt', 'utf-8');
+
+// Split data by commas to get individual ranges
+const ranges = data.split(',');
+let invalidIds = [];
+
+ranges.forEach((range) => {
+    const [start, end] = range.split('-').map(Number);
+    for (let id = start; id <= end; id++) {
+        const idStr = id.toString();
+        if (idStr.length % 2 === 0) {
+            const idStrFirstHalf = idStr.slice(0, idStr.length / 2);
+            const idStrSecondHalf = idStr.slice(idStr.length / 2);
+            if (idStrFirstHalf === idStrSecondHalf) {
+                invalidIds.push(id);
+            }
+        }
+    }
+});
+
+console.log('Invalid IDs:', invalidIds);
+console.log('Total Invalid IDs:', invalidIds.length);
+console.log('Sum Invalid IDs:', invalidIds.map(Number).reduce((a, b) => a + b, 0));
+
+// OUTPUT
+
+// Invalid IDs: [
+//   200200, 201201, 202202, 203203, 204204, 205205, 206206,
+//   207207, 208208, 209209, 210210, 211211, 212212, 213213,
+//   214214, 215215, 216216, 217217, 218218, 219219, 220220,
+//   221221, 222222, 223223, 224224, 225225, 226226, 227227,
+//   228228, 229229, 230230, 231231, 232232, 233233, 234234,
+//   235235, 236236, 237237, 238238, 239239, 240240, 241241,
+//   242242, 243243, 244244, 245245, 246246, 247247, 248248,
+//   249249, 250250, 251251, 252252, 253253, 254254, 963963,
+//   964964, 965965, 966966, 967967, 968968, 969969, 970970,
+//   971971, 972972, 973973, 974974, 975975, 976976, 977977,
+//   978978, 979979, 980980, 981981, 982982, 983983, 984984,
+//   985985, 986986, 987987, 988988, 989989, 990990, 991991,
+//   992992, 993993, 994994, 995995, 996996, 997997, 998998,
+//   999999,     44,     55,     66,     77, 100100, 101101,
+//   102102, 103103,
+//   ... 472 more items
+// ]
+// Total Invalid IDs: 572
+// Sum Invalid IDs: 9188031749
